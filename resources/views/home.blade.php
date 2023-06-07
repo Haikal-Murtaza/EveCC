b52203d21e27cc4689666862cd0f62cbd361c5f6
Haikal-Murtaza <hm602761@gmail.com>
2023-06-07 15:34:11 +0700

    laravel start

---
+++ resources/views/home.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
+    <title>Halaman Home</title>
+</head>
+
+<body>
+    <div class="container-fluid text-center">
+        <div class="row align-items-start">
+            <div class="col bg-dark px-3 py-1 text-white d-inline-block">
+                One of three columns
+            </div>
+            <div class="col bg-dark px-3 py-1 text-white d-inline-block">
+                One of three columns
+            </div>
+            <div class="col bg-dark px-3 py-1 text-white d-inline-block">
+                One of three columns
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
