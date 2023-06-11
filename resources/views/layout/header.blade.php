acbb06a3ec89afb79b14097142e4c0dc2c4fce87
Haikal-Murtaza <hm602761@gmail.com>
2023-06-11 22:02:30 +0700

    halaman home

---
+++ resources/views/layout/header.blade.php
@@ -0,0 +1,18 @@
+<nav class="navbar navbar-expand-lg stiky-top p-4 bg-dark-subtle mb-5">
+    <div class="container-fluid">
+        <a class=" navbar-brand d-flex ms-5 text-dark text-decoration-none" href="/">
+            EveCC
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse justify-content-end me-5" id="navbarSupportedContent">
+            <ul class="navbar-nav jus">
+                <li class="nav-item me-2"><a href="#" class="nav-link active text-decoration-underline" aria-current="page">Home</a></li>
+                <li class="nav-item me-2"><a href="#" class="nav-link">Recipe</a></li>
+                <li class="nav-item me-2"><a href="#" class="nav-link">Share Idea</a></li>
+                <li class="nav-item"><a href="#" class="nav-link">Join Us</a></li>
+            </ul>
+        </div>
+</nav>
